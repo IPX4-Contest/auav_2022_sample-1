c8135ed05bee38cf27bb91a89e28d33e812e2a5d
Justin Cooper <coope263@purdue.edu>
2022-02-27 14:32:11 -0500

    fixed bugs

--- tracking_car/scripts/find_rel_pos.py
+++ tracking_car/scripts/find_rel_pos.py
@@ -26,20 +26,21 @@ from sensor_msgs.msg import PointField
 
 class Find_pos_goal:
 
-    def __init__(self, centerRed, drone_pose_x, drone_pose_y, drone_pose_z):
+    def __init__(self, centerRed, drone_pose_x, drone_pose_y, drone_pose_z, depth_array):
         self.dist_from_pointcloud = 1
 
-        self.eg_per_pix = 120/640   # deg/pixels
+        self.deg_per_pix = 120/640   # deg/pixels
         self.hor_pix_from_center = centerRed[0] - 320
-        self.hor_degree = deg_per_pix*hor_pix_from_center
-        self.hor_rad = hor_degree*(3.14159/180.0)
+        self.hor_degree = self.deg_per_pix*self.hor_pix_from_center
+        self.hor_rad = self.hor_degree*(3.14159/180.0)
 
-        self.delta_x = np.cos(hor_rad)*dist_from_pointcloud
-        self.delta_y = -np.sin(hor_rad)*dist_from_pointcloud
+        self.delta_x = np.cos(self.hor_rad)*self.dist_from_pointcloud
+        self.delta_y = -np.sin(self.hor_rad)*self.dist_from_pointcloud
 
-        self.depth_array
+        self.depth_array = depth_array
 
         #goal_rel_pose message initialization
+        self.pose = PoseStamped()
         self.pose.header.seq = 3
         self.pose.header.stamp = rospy.Time.now()
         self.pose.header.frame_id = "map"
@@ -51,8 +52,8 @@ class Find_pos_goal:
     def pos_goal(self):
         # self.x_manual += self.x_step
         # pose.pose.position.x = self.x_manual
-        self.pose.pose.position.x = self.drone_pose_x + delta_x
-        self.pose.pose.position.y = self.drone_pose_y + delta_y
+        self.pose.pose.position.x = self.drone_pose_x + self.delta_x
+        self.pose.pose.position.y = self.drone_pose_y + self.delta_y
         self.pose.pose.position.z = 0.0
 
         self.pose.pose.orientation.x = 0.0
@@ -62,8 +63,8 @@ class Find_pos_goal:
         self.rate = rospy.Rate(1) # 0.2hz
         self.rate.sleep()
 
-        print("drone_x: %.2f\ndrone_y: %.2f\ndrone_z: %.2f\n" % (self.drone_pose_x, self.drone_pose_y, self.drone_pose_z))
-        print("diff_x: %.2f\ndiff_y: %.2f\n" % (delta_x, delta_y))
+        # print("drone_x: %.2f\ndrone_y: %.2f\ndrone_z: %.2f\n" % (self.drone_pose_x, self.drone_pose_y, self.drone_pose_z))
+        # print("diff_x: %.2f\ndiff_y: %.2f\n" % (self.delta_x, self.delta_y))
 
         return self.pose
 
--- tracking_car/scripts/view_gazebo_camera.py
+++ tracking_car/scripts/view_gazebo_camera.py
@@ -25,7 +25,7 @@ from sensor_msgs.msg import PointCloud2
 from sensor_msgs.msg import PointField
 
 from find_pos_rgb import Find_pos_rgb
-from find_pos_goal import Find_pos_goal
+from find_rel_pos import Find_pos_goal
 
 
 class view_gazebo_camera:
@@ -40,7 +40,7 @@ class view_gazebo_camera:
     self.drone_pose_y = 0.0
     self.drone_pose_z = 0.0
 
-    self centerRed = [0,0]
+    self.centerRed = [0,0]
 
     self.image_sub = rospy.Subscriber("/drone/camera/rgb/image_raw", Image, self.img_callback)
     self.drone_pose = rospy.Subscriber("/drone/mavros/local_position/pose", PoseStamped, self.pose_callback)
@@ -48,64 +48,68 @@ class view_gazebo_camera:
     self.image_depth_sub = rospy.Subscriber("/drone/camera/depth/points", PointCloud2, self.depth_callback, queue_size=1, buff_size=52428800)
     self.pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
 
-#update the image 
-def img_callback(self,data):
-    bridge = CvBridge()
-    pose = PoseStamped()
-
-    try:
-      image = bridge.imgmsg_to_cv2(data, desired_encoding="bgr8")
-    except CvBridgeError as e:
-      rospy.logerr(e)
-      
-    self.rgb_array = np.array(image,dtype=np.uint8)
-    print(self.rgb_array.shape)
-
-    #print(self.depth_array)
-
-    find_center = Find_pos_rgb(rgb_array)
-    #matrixRed = find_pos_rgb.findred  # rgb: numpy array of
-    self.centerRed = find_center.findCenter()
-
-    #PointCLoud = self.depth_array(#insert the index collected from center red here)
-    #then change the pose to corresond to PC
-
-    print(centerRed)
-    # print(self.drone_pose.pose.position.x)
-    # print(self.drone_pose.pose.position.y)
-    # print(self.drone_pose.pose.position.z)
-
-#update current drone position
-def pose_callback(self, data):
-  self.drone_pose_x = data.pose.position.x
-  self.drone_pose_y = data.pose.position.y
-  self.drone_pose_z = data.pose.position.z
-
-#publish the goal position to the topic
-def depth_callback(self, data):
-    print('working')
-    points_list = []
-
-    for data in pc2.read_points(data, skip_nans=True):
-      points_list.append([data[0], data[1], data[2], data[3]])
-
-    self.depth_array = np.array(points_list)
-    print(self.depth_array.shape)
-
-    find_goal = Find_pos_goal(self.centerRed, drone_pose_x, drone_pose_y, drone_pose_z)
-    pose = find_goal.pos_goal()
-
-    self.pub.publish(pose)
-    '''
+  #update the image 
+  def img_callback(self,data):
       bridge = CvBridge()
+      pose = PoseStamped()
+
       try:
-        # The depth image is a single-channel float32 image
-        depth_image = bridge.imgmsg_to_cv2(data,"32FC1")
+        image = bridge.imgmsg_to_cv2(data, desired_encoding="bgr8")
       except CvBridgeError as e:
         rospy.logerr(e)
-      
-      self.depth_array = np.array(depth_image, dtype=np.float32)
-    '''
+        
+      self.rgb_array = np.array(image,dtype=np.uint8)
+      print(f"self.rgbarray.shape: {self.rgb_array.shape}")
+
+      #print(self.depth_array)
+
+      find_center = Find_pos_rgb(self.rgb_array)
+      #matrixRed = find_pos_rgb.findred  # rgb: numpy array of
+      self.centerRed = find_center.findCenter()
+
+      #PointCLoud = self.depth_array(#insert the index collected from center red here)
+      #then change the pose to corresond to PC
+
+      print(f"self.centerRed: {self.centerRed}")
+      # print(self.drone_pose.pose.position.x)
+      # print(self.drone_pose.pose.position.y)
+      # print(self.drone_pose.pose.position.z)
+
+  #update current drone position
+  def pose_callback(self, data):
+    self.drone_pose_x = data.pose.position.x
+    self.drone_pose_y = data.pose.position.y
+    self.drone_pose_z = data.pose.position.z
+
+  #publish the goal position to the topic
+  def depth_callback(self, data):
+      print('working')
+      points_list = []
+
+      for data in pc2.read_points(data, skip_nans=True):
+        points_list.append([data[0], data[1], data[2], data[3]])
+
+      self.depth_array = np.array(points_list)
+      print(self.depth_array.shape)
+
+      find_goal = Find_pos_goal(self.centerRed, self.drone_pose_x, self.drone_pose_y, self.drone_pose_z, self.depth_array)
+      pose = find_goal.pos_goal()
+
+      self.pub.publish(pose)
+
+
+
+
+      '''
+        bridge = CvBridge()
+        try:
+          # The depth image is a single-channel float32 image
+          depth_image = bridge.imgmsg_to_cv2(data,"32FC1")
+        except CvBridgeError as e:
+          rospy.logerr(e)
+        
+        self.depth_array = np.array(depth_image, dtype=np.float32)
+      '''
   
 def main():
   view_gazebo_camera()
@@ -128,7 +132,7 @@ if __name__ == '__main__':
     #print(depth_array.shape)
 
     #self.image_pub.publish(numpy_array)
-    #cv2.imshow("Camera output rgb", image)
+    ##cv2.imshow("Camera output rgb", image)
     #cv2.waitKey(3)
 
         #print(numpy_array.shape)
